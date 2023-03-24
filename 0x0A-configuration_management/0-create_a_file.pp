b3467df5a1bdacd1caac601ed0bf60f7f1962471
Cheserem Titus <cheseremtitus24@gmail.com>
2023-03-24 20:05:13 +0300

    modified:   0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 file { '/tmp/school':
-  ensure  => file,
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => "I love Puppet",
+  ensure   => file,
+  mode     => '0744',
+  owner    => 'www-data',
+  group    => 'www-data',
+  content  => 'I love Puppet',
+  checksum => 'md5',
 }
