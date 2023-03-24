2d8aec5533ee3a399d56d7825f4c28d34dfd3140
Cheserem Titus <cheseremtitus24@gmail.com>
2023-03-24 20:13:36 +0300

    modified:   1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,10 @@
+# Installs flask python package
 package { 'python3-pip':
   ensure => installed,
 }
 
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask',
+  command => '/usr/bin/pip3 install flask',
   path    => ['/usr/bin', '/usr/local/bin'],
   require => Package['python3-pip'],
 }
