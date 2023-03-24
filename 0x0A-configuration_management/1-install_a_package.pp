909093b947c25a61c81fcaf81237f7f74c12ce8c
Cheserem Titus <cheseremtitus24@gmail.com>
2023-03-24 21:44:31 +0300

    modified:   1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Installs flask python package
+# Installs flask python package using pip3 installer
 package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3.8',
+  ensure   => 'latest',
+  provider => 'pip3',
 }
 
