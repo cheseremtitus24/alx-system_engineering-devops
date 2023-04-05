ba5af25f0b405fa9c0b98205f4e98b3fea47580f
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-04-05 06:05:54 +0300

    modified:   2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,8 +3,8 @@ include stdlib
 $header = 'add_header X-Served-By $hostname;'
 
 file_line { 'add_custom_header':
-  #path => '/etc/nginx/sites-enabled/default',
-  path => '/home/cheserem/Desktop/alx-system_engineering-devops/0x0F-load_balancer/default',
+  path => '/etc/nginx/sites-enabled/default',
+  #path => '/home/cheserem/Desktop/alx-system_engineering-devops/0x0F-load_balancer/default',
   line => $header,
 }
 
