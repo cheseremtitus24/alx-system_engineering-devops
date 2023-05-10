cf500114db3fde36aa5317df2d4f56a87c949dd3
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-05-10 15:04:14 +0300

    Docs

---
+++ 0x16-api_advanced/post.sh
@@ -0,0 +1,1 @@
+curl -X POST $url -H "Content-Type: application/json" -d {query: programming} -A "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
