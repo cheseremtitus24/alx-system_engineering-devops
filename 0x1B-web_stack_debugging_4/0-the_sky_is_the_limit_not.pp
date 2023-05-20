ca82df924f9410fac894648edb3c445d25ea586c
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-05-20 12:08:51 +0300

    Updated: Open File Limit from 4096 to 7000

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Increases the Limit of File Descriptors.
 exec { 'mod_nginx_config':
-  command => 'sed -i s/^ULIMIT.*/ULIMIT="\"-n 4096\""/g /etc/default/nginx',
+  command => 'sed -i s/^ULIMIT.*/ULIMIT="\"-n 7000\""/g /etc/default/nginx',
   onlyif      => 'test -e /etc/default/nginx',
   path    => ['/bin', '/usr/bin'],
   notify  => Service['nginx'],
