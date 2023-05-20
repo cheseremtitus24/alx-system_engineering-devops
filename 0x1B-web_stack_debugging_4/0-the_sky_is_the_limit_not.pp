0fb4509790be788b96d0290f1e499843ab50b0e6
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-05-20 12:10:36 +0300

    Refactored: puppet-lint compliance

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # Increases the Limit of File Descriptors.
 exec { 'mod_nginx_config':
   command => 'sed -i s/^ULIMIT.*/ULIMIT="\"-n 7000\""/g /etc/default/nginx',
-  onlyif      => 'test -e /etc/default/nginx',
+  onlyif  => 'test -e /etc/default/nginx',
   path    => ['/bin', '/usr/bin'],
   notify  => Service['nginx'],
   require => Package['nginx'],
